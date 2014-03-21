ae0327bbb67e0aa7bc3f42259d1d37635dd484dc
Christopher Bunting <christophersbunting@gmail.com>
2014-03-20 22:29:00 -0700

    Tighten this equation up

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -10,7 +10,7 @@ void loop()
   float degreesF;
 
   // Read the pin output, convert to volts, then to C, then to F
-  degreesF = (((analogRead(pin) * 0.004882814) - 0.5) * 100) * (9.0/5.0) + 32.0;
+  degreesF = (((analogRead(pin) * 0.004882814) - 0.5) * 180) + 32.0;
   
   Serial.print("Degrees F: ");
   Serial.println(degreesF);
